0ae1452367a3364cd47311ff0bd87be7aea07df3
Kader64 <w.lichton.kader@gmail.com>
2026-04-16 19:50:18 +0200

    prepared data for neural network

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ images, labels = mndata.load_training()
 
 images = np.array(images)
 labels = np.array(labels)
+countImg, sizeImg = images.shape
 
-m, n = images.shape
-print(m, n)
+# Splitting data into validation and training set
+data_val = images[0:1000]
+label_val = labels[0:1000]
+data_val = data_val / 255
+
+data_train = images[1000:]
+label_train = labels[1000:]
+data_train = data_train / 255
 
 
-#print(mndata.display(images[0]))
