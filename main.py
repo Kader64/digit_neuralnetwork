7b049a397b576c6c2578a9cfae98ad462816632b
Kader64 <w.lichton.kader@gmail.com>
2026-04-16 19:14:34 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from mnist import MNIST
+
+# Loading data from files
+mndata = MNIST('data')
+images, labels = mndata.load_training()
+
+images = np.array(images)
+labels = np.array(labels)
+
+m, n = images.shape
+print(m, n)
+
+
+
+#print(mndata.display(images[0]))
